d0202f4c7c50cee08f725108fb0f157b6b5e455f
Dani <danielymelo@aluno.uespi.br>
2026-09-23 01:30:00 -0300

    add docker

--- DevShowcase/src/main/java/com/example/DevShowcase/repository/TechnologyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.DevShowcase.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.DevShowcase.model.Technology;
-
-@Repository
-public interface TechnologyRepository extends JpaRepository<Technology, Long> {
-}
